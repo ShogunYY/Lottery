a38f2398f1250d540d7a55596de0584ad20d2ac9
ShogunYY <hanshinnomiya@gmail.com>
2021-04-15 17:18:48 -0700

    添加gift dao层的crud

---
+++ dao/gift_dao.go
@@ -0,0 +1,71 @@
+package dao
+
+import (
+	"github.com/go-xorm/xorm"
+	"log"
+	models "lottery/model"
+)
+
+type GiftDao struct {
+	engine *xorm.Engine
+}
+
+func newGiftDao(engine *xorm.Engine) *GiftDao {
+	return &GiftDao{
+		engine: engine,
+	}
+}
+
+func (d *GiftDao) Get(id int) *models.LtGift {
+
+	data := &models.LtGift{Id: id}
+	ok, err := d.engine.Get(data)
+	if ok && err == nil {
+		return data
+	}
+
+	data.Id = 0
+	return data
+}
+
+func (d *GiftDao) GetAll() []models.LtGift {
+
+	dataList := make([]models.LtGift, 0)
+	err := d.engine.Asc("sys_status").Asc("displayorder").Find(&dataList)
+	if err != nil {
+		log.Println("gift_dao GetAll() error: ", err)
+		return dataList
+	}
+
+	return dataList
+}
+
+func (d *GiftDao) CountAll() int64 {
+
+	count, err := d.engine.Count(&models.LtGift{})
+	if err != nil {
+		log.Println("gift_dao CountAll() error: ", err)
+		return 0
+	}
+
+	return count
+}
+
+func (d *GiftDao) Delete(id int) error {
+
+	data := &models.LtGift{Id: id, SysStatus: 1}
+	_, err := d.engine.Id(data.Id).Update(data)
+	return err
+}
+
+func (d *GiftDao) Update(data *models.LtGift, columns []string) error {
+
+	_, err := d.engine.Id(data.Id).MustCols(columns...).Update(data)
+	return err
+}
+
+func (d *GiftDao) Create(data *models.LtGift) error {
+
+	_, err := d.engine.Insert(data)
+	return err
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module lottery
+
+go 1.16
+
+require (
+	github.com/Joker/hpp v1.0.0 // indirect
+	github.com/go-xorm/cmd/xorm v0.0.0-20190426080617-f87981e709a1 // indirect
+	github.com/kataras/iris/v12 v12.2.0-alpha2.0.20210413181054-382e7c14cbd3
+	github.com/nats-io/nats-server/v2 v2.2.1 // indirect
+	github.com/smartystreets/goconvey v1.6.4 // indirect
+)
